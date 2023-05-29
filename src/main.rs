1ae4e261ae96be49534d691e876a39b62de66b8a
Johannes Maier <johannes.maier@mailbox.org>
2023-05-29 10:25:38 +0200

    Add player movement

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,9 @@ impl<'a> System<'a> for LeftWalker {
     }
 }
 
+#[derive(Component, Debug)]
+struct Player {}
+
 struct State {
     ecs: World,
 }
@@ -42,6 +45,7 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
 
+        player_input(self, ctx);
         self.run_systems();
 
         let positions = self.ecs.read_storage::<Position>();
@@ -60,6 +64,29 @@ impl State {
     }
 }
 
+fn try_move_player(dx: i32, dy: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + dx));
+        pos.y = min(49, max(0, pos.y + dy));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {}
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
+    }
+}
+
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
@@ -69,6 +96,7 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
     gs.ecs
         .create_entity()
         .with(Position { x: 40, y: 25 })
@@ -77,6 +105,7 @@ fn main() -> rltk::BError {
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
+        .with(Player {})
         .build();
 
     for i in 0..10 {
